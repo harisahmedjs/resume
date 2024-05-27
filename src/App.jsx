33b69710d2d2642e0173351e3d6e43aefaf757dc
harisahmedjs <harisahsan477@gmail.com>
2024-05-27 11:41:05 -0700

    complete Template

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,28 @@ import image from "./assets/download.jpg"
 export default function App() {
   return (
     <>
-      <div className='text-center bg-pink-700 text-pink-700'>
+      <div className='text-center bg-pink-700 text-pink-700 w-[1123] '>
         hello
       </div>
-      <div className='ml-10'>
+      <div className=' ml-28'>
 
 
-        <div className='flex flex-row mt-16  '>
+        <div className='flex flex-row mt-16 text-[17px]'>
           <div>
-            <img className='rounded-full w-[500px]' src={image} alt="" />
-            <div className='mt-[250px]'>
-            <p>[Recipient City, ST Zip]</p>
-                <p>email</p>
-                <p>website</p>
-                <p>phone</p>
+            <img className='rounded-full w-[250px]' src={image} alt="" />
+            <div className='mt-[320px] text-2xl text-pink-700'>
+            <p className='ml-8 mb-3' ><i class="ri-map-pin-line mr-2 text-pink-900"></i>Recipient City, ST Zip</p>
+                <p  className='ml-8 mb-3' ><i class="ri-mail-line mr-2 text-pink-900"></i>email</p>
+                <p  className='ml-8 mb-3' ><i class="ri-global-line mr-2 text-pink-900"></i>website</p>
+                <p  className='ml-8 mb-3' ><i class="ri-phone-line mr-2 text-pink-900"></i>phone</p>
                 </div>
           </div>
 
-          <div className='ml-32'>
-            <h1 className='text-4xl text-pink-700 font-medium'>[First Name]</h1>
+          <div className=' w-[650px] ml-40'>
+            <h1 className='text-4xl text-pink-700 font-medium'>First Name</h1>
 
-            <h1 className='text-4xl text-pink-700 font-medium mt-3'> [Surname]</h1>
-            <hr className='w-[700px] h-[6px] mt-9 font-bold bg-pink-700' />
+            <h1 className='text-4xl text-pink-700 font-medium mt-3'> Surname</h1>
+            <hr className='w-[650px] h-[6px] mt-9 font-bold bg-pink-700' />
             <h3 className='text-xl mt-8 mb-8'>Date</h3>
             <p>[Recipient Name]</p>
             <div className='flex flex-row'>
@@ -39,7 +39,7 @@ export default function App() {
             </div>
 
 
-            <hr className='w-[700px] h-[6px] mt-9 mb-9 font-bold bg-pink-700' />
+            <hr className='w-[650px] h-[6px] mt-9 mb-9 font-bold bg-pink-700' />
 
             <h3 className='text-3xl font-normal text-pink-700'>Dear Recipient Name</h3>
             <p className='mt-9'>Recipient Street Address Recipient City, ST Zip
@@ -48,7 +48,7 @@ export default function App() {
            
                <p className='mt-7'> It's easy to match any of the text formatting you see here. On the Home tab of the ribbon, check out the Styles gallery for all styles used in this letter.</p>
 
-                <p className='mt-4'>Sincerely, <br /> Your Name</p>
+                <p className='mt-6'>Sincerely, <br />  Your Name</p>
               
             
 
